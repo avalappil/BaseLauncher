16123783fdcdfe744ccb860000a110f09bb4bab8
Ajith Valappil <ajith.vv@gmail.com>
2018-04-28 16:54:27 -0400

    initial

---
+++ app/src/main/java/com/baselauncher/ajithvalappil/baselauncher/AppDetail.java
@@ -0,0 +1,12 @@
+package com.baselauncher.ajithvalappil.baselauncher;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by ajithvalappil2 on 8/7/16.
+ */
+public class AppDetail {
+    CharSequence label;
+    CharSequence name;
+    Drawable icon;
+}
